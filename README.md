a797f1af79ebabaff288f1cd8f6e00436e24bd4a
cule925 <iwanculumovic5555@gmail.com>
2026-01-13 23:55:07 +0100

    Add handling for a second I2C slave

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Raspberry Pi 4B SHA256 calculator
 
-Clone this repository in your home directory on your Raspberry Pi running Raspberry Pi OS.
+Clone this repository in your home directory on your Raspberry Pi 4B running Raspberry Pi OS.
 
 Copy the `config.txt` file on the boot partition with the command:
 
@@ -8,7 +8,7 @@ Copy the `config.txt` file on the boot partition with the command:
 sudo cp ~/rpi4b-i2c-master-sha256-calculator/firmware/config.txt /boot/firmware/config.txt
 ```
 
-This will enable hardware I2C on GPIO 2 and GPIO 3. Also, it will enable interrupt on GPIO 25. The device file for the interrupt is `/dev/input/event4`.
+This will enable hardware I2C on GPIO 2 and GPIO 3 where the Raspberry Pi will be running as an I2C master. Also, it will enable interrupt on GPIO 24 and GPIO 25 with which the I2C slaves will signal that data is ready. The device files for the interrupts will be `/dev/input/event0` and `/dev/input/event1`. The addresses of the I2C slaves need to be `0x08` and `0x09` and need to match the ordering of the GPIO interrupt pins (GPIO 24 for I2C device address 0x08 and GPIO 25 for I2C device address 0x09).
 
 Also, enable auto-loading the I2C device driver using the following command:
 
@@ -22,8 +22,10 @@ Install the following Python packages system wide:
 sudo apt install python3-evdev python3-smbus2
 ```
 
-Start the app `main.py` by running:
+Power off your Raspberry Pi I2C master, connect the I2C slaves to it and power them up and then power up the Raspberry Pi. After the Raspberry Pi boots, start the app `main.py` by running:
 
 ```
 python3 ~/rpi4b-i2c-master-sha256-calculator/app/main.py
 ```
+
+To dismantle everything first power off the Raspberry Pi I2C master and then power off the I2C slaves. After that disconnect the wires.
--- app/main.py
+++ app/main.py
@@ -4,16 +4,21 @@ import os
 import hashlib
 from smbus2 import SMBus, i2c_msg
 from evdev import InputDevice, ecodes, categorize
+import select
+import time
 
 # ----------------------------------------
 I2C_BUS = 1
 
 EVENT_DEVICE_1_I2C_ADDR = 0x08
+EVENT_DEVICE_2_I2C_ADDR = 0x09
 
 EVENT_DEVICE_1 = "/dev/input/event0"
+EVENT_DEVICE_2 = "/dev/input/event1"
 
 SHA256_MASK = 15
-OFFSET_1 = 0x80000000
+OFFSET_1 = 0x00000000
+OFFSET_2 = 0x00010000
 # ----------------------------------------
 
 def generate_sha256():
@@ -23,34 +28,41 @@ def generate_sha256():
 
 def send_i2c_payload(bus, address, offset, mask, digest):
     payload = bytearray()
-    payload.extend(offset.to_bytes(4, "big"))
-    payload.extend(mask.to_bytes(1, "big"))
+    payload.extend(offset.to_bytes(4, "little"))
+    payload.extend(mask.to_bytes(1, "little"))
     payload.extend(digest)
 
     assert len(payload) == 37
 
-    msg = i2c_msg.write(address, payload)
-    bus.i2c_rdwr(msg)
+    payload_msg = i2c_msg.write(address, payload)
+    bus.i2c_rdwr(payload_msg)
 
-    print("I2C: written 37 bytes")
+    print(f"I2C: written 37 bytes to {hex(address)}")
 
-def wait_for_event(dev):
-    print(f"Waiting for button press on path: {dev.path}")
+def wait_for_events(devices):
 
-    for event in dev.read_loop():
-        if event.type == ecodes.EV_KEY:
-            key_event = categorize(event)
+    print("Waiting for button events on devices:", [d.path for d in devices])
 
-            if key_event.keystate == key_event.key_down:
-                print("Event level low detected")
-                return
+    # Get the file descriptors
+    fds = [dev.fd for dev in devices]
+
+    while True:
+        rlist, _, _ = select.select(fds, [], [])
+        for fd in rlist:
+            dev = next(d for d in devices if d.fd == fd)
+            for event in dev.read():
+                if event.type == ecodes.EV_KEY:
+                    key_event = categorize(event)
+                    if key_event.keystate == key_event.key_down:
+                        print("Event detected on {dev.path}")
+                        return dev
 
 def read_i2c_response(bus, address):
     msg = i2c_msg.read(address, 4)
     bus.i2c_rdwr(msg)
 
     data = bytes(msg)
-    print(f"I2C: read 4 bytes")
+    print(f"I2C: read 4 bytes from {hex(address)}")
     return data
 
 def main():
@@ -60,17 +72,26 @@ def main():
     print(f"Mask: {mask}")
 
     dev_1 = InputDevice(EVENT_DEVICE_1)
-    dev_1_i2c_addr = EVENT_DEVICE_1_I2C_ADDR
-    offset_1 = OFFSET_1
+    dev_2 = InputDevice(EVENT_DEVICE_2)
+
+    devices = [dev_1, dev_2]
+    i2c_addresses = [EVENT_DEVICE_1_I2C_ADDR, EVENT_DEVICE_2_I2C_ADDR]
+    offsets = [OFFSET_1, OFFSET_2]
 
     with SMBus(I2C_BUS) as bus:
-        send_i2c_payload(bus, dev_1_i2c_addr, offset_1, mask, digest)
-        wait_for_event(dev_1)
-        value_bytes = read_i2c_response(bus, dev_1_i2c_addr)
+
+        for i, dev in enumerate(devices):
+            send_i2c_payload(bus, i2c_addresses[i], offsets[i], mask, digest)
+            time.sleep(0.001)
+
+        triggered_dev = wait_for_events(devices)
+        idx = devices.index(triggered_dev)
+        i2c_addr = i2c_addresses[idx]
+        value_bytes = read_i2c_response(bus, i2c_addr)
         value = int.from_bytes(value_bytes, "little")
         value_digest = hashlib.sha256(value_bytes).digest()
-        print(f"Value: {value}")
-        print(f"Value digest: {value_digest.hex()}")
+        print(f"Device {triggered_dev.path} value: {value}")
+        print(f"Device {triggered_dev.path} value digest: {value_digest.hex()}")
 
 if __name__ == "__main__":
     main()
